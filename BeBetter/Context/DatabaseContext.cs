48a2916367b619857ec390b70c1c76eea65a6ae6
Alice Gayduk <simara@redwerk.com>
2017-04-05 22:35:40 +0300

    add missing files

---
+++ BeBetter/Context/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BeBetter.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() 
+            : base("Database1")
+        {
+           Database.SetInitializer<DatabaseContext>(null);
+        }
+
+        public DbSet<ArticleModel> Articles { get; set; }
+    }
+}
---
+++ BeBetter/Models/ArticleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BeBetter.Models
+{
+    public class ArticleModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+        [Display(Name = "Body")]
+        public string Body { get; set; }
+    }
+}
